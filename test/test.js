450e1277d2854110d35cce06a6eeb977dfab6d7d
rhysd <lin90162@yahoo.co.jp>
2018-07-30 12:56:35 +0900

    do not use deprecated methods in 'assert' module

--- test/test.js
+++ test/test.js
@@ -1,34 +1,34 @@
-const assert = require('assert');
+const { strictEqual: eq, notStrictEqual: neq } = require('assert');
 const CLIEngine = require('eslint').CLIEngine;
 
 describe('eslint-config-fixable-all', function() {
-    beforeEach(function () {
+    beforeEach(function() {
         this.cli = new CLIEngine({
             useEslintrc: false,
-            configFile: 'eslintrc.json'
+            configFile: 'eslintrc.json',
         });
     });
 
-    afterEach(function () {
+    afterEach(function() {
         delete this.cli;
     });
 
-    it('detects fixable error', function () {
+    it('detects fixable error', function() {
         const code = 'var foo = 42';
         const checked = this.cli.executeOnText(code);
-        assert.notEqual(checked.errorCount, 0);
-        assert.equal(checked.warningCount, 0);
+        neq(checked.errorCount, 0);
+        eq(checked.warningCount, 0);
 
         const ids = checked.results[0].messages.map(m => m.ruleId);
-        assert.notEqual(ids.indexOf('no-var'), -1);
-        assert.notEqual(ids.indexOf('eol-last'), -1);
-        assert.notEqual(ids.indexOf('semi'), -1);
+        neq(ids.indexOf('no-var'), -1);
+        neq(ids.indexOf('eol-last'), -1);
+        neq(ids.indexOf('semi'), -1);
     });
 
-    it('does not detect non-fixable error', function () {
+    it('does not detect non-fixable error', function() {
         const code = 'console.log("test");\n';
         const checked = this.cli.executeOnText(code);
-        assert.equal(checked.errorCount, 0);
-        assert.equal(checked.warningCount, 0);
+        eq(checked.errorCount, 0);
+        eq(checked.warningCount, 0);
     });
 });
